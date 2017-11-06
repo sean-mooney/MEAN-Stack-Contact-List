f9d565e2bd65bcfea8a506ce0128c9e2e8e3d481
Sean Mooney <smooney914@gmail.com>
2017-11-06 12:15:12 -0500

    Initial Commit

---
+++ client/src/app/contact.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import {Contact} from './contact';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ContactService {
+
+  constructor(private http: Http) { }
+
+  //retrieving contacts
+  getContacts() {
+    return this.http.get('http://localhost:8080/api/contacts').map(res => res.json());
+  }
+
+  //add contact method
+  addContact(newContact) {
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post('http://localhost:8080/api/contact', newContact,{headers:headers}).map(res => res.json());
+  }
+
+  //delete method
+  deleteContact(id) {
+    return this.http.delete('http://localhost:8080/api/contact/'+id).map(res => res.json());
+  }
+}
